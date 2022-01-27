32a812f654e05636b871006fde4d128991a4d175
torryjini <torryjini@users.noreply.github.com>
2022-01-27 17:26:06 +0900

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -1,8 +1,8 @@
 const product = [
     {
         hrefNumber : 1,
-        thumbnail : "assets/img/product/thumbnail/001.jpg",
-        imgSrc : "assets/img/product/001.jpg",
+        thumbnail : "assets/img/product/thumbnail/thumbnail_001.jpg",
+        imgSrc : "assets/img/product/detail_001.jpg",
         summary : "기내용",
         price : "3,000",
         size : 20,
@@ -13,8 +13,8 @@ const product = [
     },
     {
         hrefNumber : 2,
-        thumbnail : "assets/img/product/thumbnail/002.jpg",
-        imgSrc : "assets/img/product/002.jpg",
+        thumbnail : "assets/img/product/thumbnail/thumbnail_002.jpg",
+        imgSrc : "assets/img/product/detail_002.jpg",
         summary : "기내용",
         price : "3,000",
         size : 20,
@@ -25,8 +25,8 @@ const product = [
     },
     {
         hrefNumber : 3,
-        thumbnail : "assets/img/product/thumbnail/003.jpg",
-        imgSrc : "assets/img/product/003.jpg",
+        thumbnail : "assets/img/product/thumbnail/thumbnail_003.jpg",
+        imgSrc : "assets/img/product/detail_003.jpg",
         summary : "화물용/중형",
         price : "4,000",
         size : 24,
@@ -37,8 +37,8 @@ const product = [
     },
     {
         hrefNumber : 4,
-        thumbnail : "assets/img/product/thumbnail/004.jpg",
-        imgSrc : "assets/img/product/004.jpg",
+        thumbnail : "assets/img/product/thumbnail/thumbnail_004.jpg",
+        imgSrc : "assets/img/product/detail_004.jpg",
         summary : "기내용",
         price : "3,000",
         size : 20,
@@ -49,8 +49,8 @@ const product = [
     },
     {
         hrefNumber : 5,
-        thumbnail : "assets/img/product/thumbnail/005.jpg",
-        imgSrc : "assets/img/product/005.jpg",
+        thumbnail : "assets/img/product/thumbnail/thumbnail_005.jpg",
+        imgSrc : "assets/img/product/detail_005.jpg",
         summary : "기내용",
         price : "2,000",
         size : 20,
@@ -61,8 +61,8 @@ const product = [
     },
     {
         hrefNumber : 6,
-        thumbnail : "assets/img/product/thumbnail/006.jpg",
-        imgSrc : "assets/img/product/006.jpg",
+        thumbnail : "assets/img/product/thumbnail/thumbnail_006.jpg",
+        imgSrc : "assets/img/product/detail_006.jpg",
         summary : "기내용",
         price : "2,000",
         size : 20,
@@ -73,8 +73,8 @@ const product = [
     },
     {
       hrefNumber : 7,
-      thumbnail : "assets/img/product/thumbnail/007.jpg",
-      imgSrc : "assets/img/product/007.jpg",
+      thumbnail : "assets/img/product/thumbnail/thumbnail_007.jpg",
+      imgSrc : "assets/img/product/detail_007.jpg",
       summary : "기내용",
       price : "2,000",
       size : 20,
@@ -85,8 +85,8 @@ const product = [
   },
   {
     hrefNumber : 8,
-    thumbnail : "assets/img/product/thumbnail/008.jpg",
-    imgSrc : "assets/img/product/008.jpg",
+    thumbnail : "assets/img/product/thumbnail/thumbnail_008.jpg",
+    imgSrc : "assets/img/product/detail_008.jpg",
     summary : "화물용/중형",
     price : "3,000",
     size : 24,
@@ -97,36 +97,99 @@ const product = [
   },
   {
     hrefNumber : 9,
-    thumbnail : "assets/img/product/thumbnail/009.jpg",
-    imgSrc : "assets/img/product/009.jpg",
+    thumbnail : "assets/img/product/thumbnail/thumbnail_009.jpg",
+    imgSrc : "assets/img/product/detail_009.jpg",
     summary : "기내용/소형",
     price : "3,000",
     size : 20,
     details : "사용감 거의 없는 좋은 제품입니다.",
     status : "대여가능",
-    wheels : "4"
+    wheels : "4",
+    password : "000"
   },
   {
     hrefNumber : 10,
-    thumbnail : "assets/img/product/thumbnail/010.jpg",
-    imgSrc : "assets/img/product/010.jpg",
+    thumbnail : "assets/img/product/thumbnail/thumbnail_010.jpg",
+    imgSrc : "assets/img/product/detail_010.jpg",
     summary : "미니캐리어",
     price : "2,000",
     size : 14,
     details : "새 제품입니다.",
     status : "대여가능",
-    wheels : "0"
+    wheels : "0",
+    password : "000"
   },
   {
     hrefNumber : 11,
-    thumbnail : "assets/img/product/thumbnail/011.jpg",
-    imgSrc : "assets/img/product/011.jpg",
+    thumbnail : "assets/img/product/thumbnail/thumbnail_011.jpg",
+    imgSrc : "assets/img/product/detail_011.jpg",
     summary : "미니캐리어",
     price : "2,000",
     size : 14,
     details : "새 제품입니다.",
     status : "대여가능",
-    wheels : "0"
+    wheels : "0",
+    password : "000"
+  },
+  {
+    hrefNumber : 12,
+    thumbnail : "assets/img/product/thumbnail/thumbnail_012.jpg",
+    imgSrc : "assets/img/product/detail_012.jpg",
+    summary : "기내용/소형",
+    price : "3,000",
+    size : 20,
+    details : "살짝 금이 간 부분이 있으나(수리예정) 사용에는 전혀 지장 없습니다.",
+    status : "대여가능",
+    wheels : "4",
+    password : "000"
+  },
+  {
+    hrefNumber : 13,
+    thumbnail : "assets/img/product/thumbnail/thumbnail_013.jpg",
+    imgSrc : "assets/img/product/detail_013.jpg",
+    summary : "화물형/중형",
+    price : "4,000",
+    size : 24,
+    details : "까진 곳과 스크래치가 조금 있으나 사용에는 전혀 지장 없습니다.",
+    status : "대여가능",
+    wheels : "4",
+    password : "000"
+  },
+  {
+    hrefNumber : 14,
+    thumbnail : "assets/img/product/thumbnail/thumbnail_014.jpg",
+    imgSrc : "assets/img/product/detail_014.jpg",
+    summary : "화물형/대형",
+    price : "5,000",
+    size : 28,
+    details : "겉면에 얼룩이 조금 묻어있으나 사용한 적이 거의 없는 좋은 제품입니다.",
+    status : "대여가능",
+    wheels : "4",
+    password : "906"
+  },
+  {
+    hrefNumber : 15,
+    thumbnail : "assets/img/product/thumbnail/thumbnail_015.jpg",
+    imgSrc : "assets/img/product/detail_015.jpg",
+    summary : "화물형/중형",
+    price : "4,000",
+    size : 24,
+    details : "아랫 부분에 까진 부분이 조금 있으나 사용에는 전혀 이상없습니다.",
+    status : "대여가능",
+    wheels : "4",
+    password : "613"
+  },
+  {
+    hrefNumber : 16,
+    thumbnail : "assets/img/product/thumbnail/thumbnail_016.jpg",
+    imgSrc : "assets/img/product/detail_016.jpg",
+    summary : "기내용/소형",
+    price : "3,000",
+    size : 20,
+    details : "사용감이 거의 없는 좋은 제품입니다.",
+    status : "대여가능",
+    wheels : "4",
+    password : "000"
   },
 ]
 
@@ -242,22 +305,21 @@ for(let i = 0; i < product.length; i++){
               <div class="col-lg-8">
                 <div class="modal-body">
                   <!-- Project summary-->
-                  <h2 class="text-uppercase">[${product[i].status}] No.${i+1}_${product[i].summary}</h2>
+                  <h3 class="text-uppercase">[${product[i].status}] No.${i+1}_${product[i].summary}</h3>
                   <br>
                   <!--<p class="item-intro text-muted">
-                    Lorem ipsum dolor sit amet consectetur.
                   </p>-->
                   <img
                     class="img-fluid d-block mx-auto"
                     src=${product[i].imgSrc}
                     alt="..."
                   />
+                    <h4>제품번호 : No.${i+1}_${product[i].summary}</h4>
                     <h4>대여여부 : ${product[i].status}</h4>
                     <h4>가격 : 하루 ${product[i].price}원</h4>
                     <h4>사이즈 : ${product[i].size}인치</h4>
                     <h4>바퀴갯수 : ${product[i].wheels}개</h4>
                     <p><strong>특징 :</strong> ${product[i].details}</p>
-                    <br>
                     <h5>현재는 문자로만 대여주문 가능하며
                     <br>한예종, 한국외대, 경희대 근처에서만 거래 가능합니다.
                     <br>양해 부탁 드립니다.<br>주문 시 제품번호를 꼭 적어주세요!</h5>
