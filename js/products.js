82a5ae6dc01713caa40d7d630453f262ddf80bbd
torryjini <torryjini@users.noreply.github.com>
2022-04-13 23:56:11 +0900

    Add files via upload

--- js/products.js
+++ js/products.js
@@ -284,10 +284,9 @@ for(let i = 0; i < product.length; i++){
                     <h4>사이즈 : ${product[i].size}인치</h4>
                     <h4>바퀴갯수 : ${product[i].wheels}개</h4>
                     <p><strong>특징 :</strong> ${product[i].details}</p>
-                    <h5>현재 문자로만 대여주문 가능합니다.
-                    <br>주문 시 제품번호를 꼭 적어주세요!</h5>
+                    <h5>주문 시 제품번호를 꼭 적어주세요!</h5>
                     <br>
-                  <a href="sms:010-9941-0158">
+                  <a href="https://hmdvfor9vvr.typeform.com/to/nDhB8iZp" target="_blank">
                     <button
                       class="btn btn-primary btn-xl text-uppercase"
                       type="button"
