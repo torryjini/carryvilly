8c7231002ae1ee446badaf8fa23b9ddde9f9b068
torryjini <torryjini@users.noreply.github.com>
2022-08-25 17:23:08 +0900

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -57,8 +57,6 @@ const orderLink = document.querySelectorAll(".order-link");
 
 window.onload = function(){
     const content = `
-        가격은 하루 대여 가격이며 대여 당일부터 청구됩니다.
-        원하는 크기의 캐리어를 무작위로 받으시면 첫 날의 대여비용이 청구되지 않습니다!
         대여 최소 3일 전에 주문 부탁드리며 당일 예약 시 취소 및 환불이 불가합니다. 
         반납일 오후 2시 전까지 반납이 원칙이며 초과시간에 따라 추가비용이 부과됩니다.`
      
